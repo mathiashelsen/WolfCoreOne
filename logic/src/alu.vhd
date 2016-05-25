8c23ae7a1e14f5014f760fcbcdbce4638b1d4854
Mathias Helsen <helsen.mathias@gmail.com>
2016-05-25 16:35:24 +0200

    first specification of ALU behaviour

--- logic/src/alu.vhd
+++ logic/src/alu.vhd
@@ -14,6 +14,21 @@ entity ALU is
 	writeback_In	    : in std_logic_vector(7 downto 0);	    -- blind copy in to out
 	writeback_Out	    : out std_logic_vector(7 downto 0);
 	registerC_In	    : in std_logic_vector(7 downto 0);	    -- blind copy in to out
-	registerC_Out	    : out std_logic_vector(7 downto 0);
+	registerC_Out	    : out std_logic_vector(7 downto 0)
     );
 end ALU;
+
+architecture default of ALU is
+begin
+    process( clk, rst )
+	if( rst = '1' ) then
+	    ALU_Out		<= X"0000_0000_0000_0000";
+	    ALU_Status_Register	<= X"0000_0000_0000_0000";
+	    writeback_Out	<= X"00";
+	    registerC_Out	<= X"00";
+	elsif (clk'event and clk = '1')
+	    writeback_Out	<= writeback_In;
+	    registerC_Out	<= registerC_In;
+	end if;
+    end process;
+end architecture;
